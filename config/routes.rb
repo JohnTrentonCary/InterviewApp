a508c5cf908743f1221ac542f263ade76e25b7dc
John Trenton Cary <johntcary@gmail.com>
2015-08-11 14:49:27 -0500

    change URL in routes.rb to be more robust

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'search/index'
   get "/auth/:provider/callback" => "sessions#create"
-  get '/auth/github' => "https://github.com/login/oauth/authorize?client_id=fc2d6213e59abadd8016&scope=repo"
+  get '/auth/github' => "https://github.com/login/oauth/authorize?client_id=#{ENV["GITHUB_KEY"]}&scope=repo"
 
   root 'search#index'
 end
